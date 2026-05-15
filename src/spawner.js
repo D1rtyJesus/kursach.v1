826dc41504ce21dd20947b12b8baa4f9e98023d4
D1rtyJesus <artemtsurkana@gmail.com>
2026-05-15 14:27:19 +0300

    Some additioan files

---
+++ src/spawner.js
@@ -0,0 +1,30 @@
+export function* enemySpawner() {
+    const types = ["Goblin", "Orc", "Skeleton", "Troll"];
+    let id = 1;
+
+    while (true) {
+        const randomType = types[Math.floor(Math.random() * types.length)];
+        const hp = Math.floor(Math.random() * 50) + 50;
+        
+        yield {
+            id: id++,
+            type: randomType,
+            hp: hp,
+            isAlive: true
+        };
+    }
+}
+
+export function timeoutIterator(iterator, timeoutSec) {
+    const startTime = Date.now();
+    const timeoutMs = timeoutSec * 1000;
+
+    while (Date.now() - startTime < timeoutMs) {
+        const nextData = iterator.next();
+        if (nextData.done) break;
+
+        const enemy = nextData.value;
+        console.log(`[Spawn] ${enemy.type} (ID: ${enemy.id}, HP: ${enemy.hp}) entered the arena!`);
+    }
+}
+//Task 1 (^_^)
