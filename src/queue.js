200fc955faa06215dc3fdb87ea59c7454e55edad
D1rtyJesus <artemtsurkana@gmail.com>
2026-05-20 16:18:25 +0300

    test Turnster test version

--- src/queue.js
+++ src/queue.js
@@ -6,10 +6,26 @@ export class Turnster {
         
     }
     enqueue(entity, speed) {
-         if (entity !== false) {
+         if (entity) {
             this.queue.push({entity: entity, priority: speed, Order: this.Counter}) 
             
             this.Counter++
          }
     }
+    dequeue() {
+
+        if (this.queue.length === 0) return null;
+
+  
+        this.queue.sort((a, b) => {
+            if (a.priority !== b.priority) {
+                return a.priority - b.priority; 
+            }
+            return b.Order - a.Order; 
+        });
+
+      
+        const fastestFighter = this.queue.pop();
+        return fastestFighter.entity;
+   }
 }
