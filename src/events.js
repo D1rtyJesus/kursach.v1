c27a15f3cfd6c3455a00313959c932ab198e7146
D1rtyJesus <artemtsurkana@gmail.com>
2026-05-20 22:22:24 +0300

    all lab 7 needs is done

--- src/events.js
+++ src/events.js
@@ -2,4 +2,25 @@ export class Events {
     constructor() {
         this.events = {}; 
     }
+
+    on(evName, cback) {
+        if (!this.events[evName]) {
+            this.events[evName] = [];
+        }
+        this.events[evName].push(cback);
+    }
+
+    off(evName, cback) {
+        if (this.events[evName]) {
+            this.events[evName] = this.events[evName].filter(cb => cb !== cback);
+        }
+    }
+
+    emit(evName, data) {
+        if (this.events[evName]) {
+            for (const cback of this.events[evName]) {
+                cback(data);
+            }
+        }
+    }
 }
