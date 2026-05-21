223c969095fc5683d1ae09b9d39adfa127a3ffd3
D1rtyJesus <artemtsurkana@gmail.com>
2026-05-21 16:54:06 +0300

    +Shields +start of effects

--- src/shields.js
+++ src/shields.js
@@ -1,1 +1,12 @@
-const shield = new Proxy(target, handler);
+export function shield(entity) {
+    return new Proxy(entity, {
+        set(target, prop, value) {
+            if (prop === 'hp' && value < target.hp) {
+                console.log("Block");
+                return true; 
+            }
+            target[prop] = value;
+            return true;
+        }
+    });
+}
