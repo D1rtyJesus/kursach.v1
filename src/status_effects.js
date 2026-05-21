4fb80e15444804deb8edd8a0fe93275d85334a48
D1rtyJesus <artemtsurkana@gmail.com>
2026-05-21 23:03:46 +0300

    hearts on fire <> lab 9 defeated

---
+++ src/status_effects.js
@@ -0,0 +1,23 @@
+export function withIgnite(entity) {
+    const ogAttack = entity.attack;
+
+    entity.attack = function(target) {
+        const bDamage = ogAttack.call(this, target);
+
+        if (!target.effects) {
+            target.effects = [];
+        }
+
+        target.effects.push({
+            name: 'Burn',
+            duration: 3, 
+            damagePerTurn: 5  
+        });
+
+        console.log(`\x1b[31m🔥 ${this.type} підпалює ${target.type}! на 3 ходи.\x1b[0m`);
+
+        return bDamage; 
+    };
+
+    return entity;
+}
